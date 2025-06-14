a8040a94462e0684ae402a0009051d6db924f29b
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-06-15 00:50:55 +0530

    Refactoring Pickup Classes

---
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public abstract class Pickup : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 100f;
+
+    const string PLAYER_STRING = "Player";
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PLAYER_STRING))
+        {
+            ActiveWeapon activeWeapon = other.GetComponentInChildren<ActiveWeapon>();
+            OnPickup(activeWeapon);
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected abstract void OnPickup(ActiveWeapon activeWeapon);
+}
